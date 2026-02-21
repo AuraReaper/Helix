10395471662415ff2b460bb563b0bec9737f13d1
Yash Kumar Gupta <yash@Yashs-MacBook-Air.local>
2026-02-21 20:18:23 +0530

    feat: Add client to send Set commands to server

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
 
 	"github.com/AuraReaper/helix/cache"
+	"github.com/AuraReaper/helix/client"
+	"github.com/AuraReaper/helix/protocol"
 )
 
 func main() {
@@ -19,6 +26,39 @@ func main() {
 		LeaderAddr: *leaderAddr,
 	}
 
+	go func() {
+		time.Sleep(2 * time.Second)
+		for i := 0; i < 10; i++ {
+			SendCommand()
+			time.Sleep(200 * time.Millisecond)
+		}
+	}()
+
 	server := NewServer(opts, cache.New())
 	server.Start()
+}
+
+func SendCommand() {
+	cmd := &protocol.CommandSet{
+		Key:   []byte("user"),
+		Value: []byte("yash"),
+	}
+
+	client, err := client.New(":3000", client.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Set(context.Background(), []byte("user"), []byte("yash"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn.Write(cmd.Bytes())
 }
