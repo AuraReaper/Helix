8758c856fb921392f8f565ad3c00733eaa1e2c41
Yash Kumar Gupta <yash@Yashs-MacBook-Air.local>
2026-02-21 20:19:43 +0530

    refactor: Use protocol package for command parsing; remove replication

--- server.go
+++ server.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/AuraReaper/helix/cache"
+	"github.com/AuraReaper/helix/protocol"
 )
 
 type ServerOpts struct {
@@ -17,16 +18,13 @@ type ServerOpts struct {
 
 type Server struct {
 	ServerOpts
-	followers map[net.Conn]struct{}
-	cache     cache.Cacher
+	cache cache.Cacher
 }
 
 func NewServer(opts ServerOpts, c cache.Cacher) *Server {
 	return &Server{
 		ServerOpts: opts,
-		// only maintained by the leader
-		followers: make(map[net.Conn]struct{}),
-		cache:     c,
+		cache:      c,
 	}
 }
 
@@ -37,18 +35,6 @@ func (s *Server) Start() error {
 	}
 
 	log.Printf("server starting on port [%s]\n", s.ListenAddr)
-
-	if !s.IsLeader {
-		go func() {
-			conn, err := net.Dial("tcp", s.LeaderAddr)
-			fmt.Printf("connected to leader: %s\n", s.LeaderAddr)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			s.handleConn(conn)
-		}()
-	}
 
 	for {
 		conn, err := ln.Accept()
@@ -63,78 +49,27 @@ func (s *Server) Start() error {
 func (s *Server) handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 2048)
-
-	if s.IsLeader {
-		s.followers[conn] = struct{}{}
-	}
-
 	fmt.Println("connection made:", conn.RemoteAddr())
 
 	for {
-		n, err := conn.Read(buf)
+		cmd, err := protocol.ParseCommand(conn)
 		if err != nil {
-			log.Printf("conn read error: %s\n", err)
+			log.Println("parse command error:", err)
 			break
 		}
-
-		go s.handleCommand(conn, buf[:n])
+		go s.handleCommand(conn, cmd)
 	}
 }
 
-func (s *Server) handleCommand(conn net.Conn, rawCmd []byte) {
-	msg, err := parseMessage(rawCmd)
-	if err != nil {
-		fmt.Println("failed to parse command", err)
-		conn.Write([]byte(err.Error()))
-		return
-	}
-
-	fmt.Printf("received command %s\n", msg.Cmd)
-
-	switch msg.Cmd {
-	case CMDSet:
-		err = s.handleSetCmd(conn, msg)
-	case CMDGet:
-		err = s.handleGetCmd(conn, msg)
-	}
-
-	if err != nil {
-		fmt.Println("failed to handle command", err)
-		conn.Write([]byte(err.Error()))
-		return
+func (s *Server) handleCommand(conn net.Conn, cmd any) {
+	switch v := cmd.(type) {
+	case *protocol.CommandSet:
+		s.handleSetCommand(conn, v)
+	case *protocol.CommandGet:
 	}
 }
 
-func (s *Server) handleGetCmd(conn net.Conn, msg *Message) error {
-	val, err := s.cache.Get(msg.Key)
-	if err != nil {
-		return err
-	}
-
-	_, err = conn.Write(val)
-
-	return nil
-}
-
-func (s *Server) handleSetCmd(conn net.Conn, msg *Message) error {
-	if err := s.cache.Set(msg.Key, msg.Value, msg.TTL); err != nil {
-		return err
-	}
-
-	go s.sendToFollowers(context.TODO(), msg)
-
-	return nil
-}
-
-func (s *Server) sendToFollowers(ctx context.Context, msg *Message) error {
-	for conn := range s.followers {
-		fmt.Println("forwarding key to follower")
-		_, err := conn.Write(msg.ToBytes())
-		if err != nil {
-			fmt.Printf("error sending message to follower:  %v", err)
-			continue
-		}
-	}
-	return nil
+func (s *Server) handleSetCommand(conn net.Conn, cmd *protocol.CommandSet) error {
+	log.Printf("SET %s to %s", cmd.Key, cmd.Value)
+	return s.cache.Set(cmd.Key, cmd.Value, time.Duration(cmd.TTL))
 }
