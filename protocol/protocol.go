4f7bd371608b03052346b8fb126c427729a207b2
Yash Kumar Gupta <yash@Yashs-MacBook-Air.local>
2026-02-22 12:36:53 +0530

    fix(protocol): Handle error from binary.Read in ParseCommand

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -56,7 +56,9 @@ func (c *CommandGet) Bytes() []byte {
 
 func ParseCommand(r io.Reader) (any, error) {
 	var cmd Command
-	binary.Read(r, binary.LittleEndian, &cmd)
+	if err := binary.Read(r, binary.LittleEndian, &cmd); err != nil {
+		return nil, err
+	}
 
 	switch cmd {
 	case CmdSet:
